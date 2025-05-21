48ccb0c4d5c8876c9cd9b7b135bd15b54165e5f8
Simonas Rupsys <simonas.rupsys@vinted.com>
2025-05-21 23:19:58 +0300

    redbench: refactor dataset

--- redbench/user.go
+++ redbench/user.go
@@ -2,48 +2,35 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"math/rand"
 	"time"
 
-	"github.com/antonputra/go-utils/util"
-	"github.com/google/uuid"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/redis/go-redis/v9"
 )
 
-type User struct {
-	Uuid      string `json:"uuid"`
-	Username  string `json:"username"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Address   string `json:"address"`
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// randomString generates a random alphanumeric string of the given length.
+func randomString(length int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = letters[seededRand.Intn(len(letters))]
+	}
+	return string(result)
 }
 
-func NewUser() *User {
-	id := uuid.New()
-
-	u := User{
-		Uuid:      id.String(),
-		Username:  util.GenString(10),
-		FirstName: util.GenString(5),
-		LastName:  util.GenString(10),
-		Address:   util.GenString(20),
-	}
-
-	return &u
-}
-
-func (u *User) SaveToRedis(ctx context.Context, rdb *redis.Client, m *metrics, exp int32, debug bool) (err error) {
-	b, err := json.Marshal(u)
-	if err != nil {
-		return util.Annotate(err, "json.Marshal failed")
-	}
+// SaveRandomToRedis generates a random key and value, saves to Redis, and returns the key.
+func SaveRandomToRedis(ctx context.Context, rdb *redis.Client, m *metrics, exp int32, debug bool, keySize, valueSize int) (key string, err error) {
+	key = randomString(keySize)
+	value := randomString(valueSize)
 
 	expr := time.Duration(exp) * time.Second
 	now := time.Now()
 
-	err = rdb.Set(ctx, u.Uuid, b, expr).Err()
+	err = rdb.Set(ctx, key, value, expr).Err()
 	if err != nil {
 		m.requestFailed.With(prometheus.Labels{"command": "set", "db": "redis", "target": host + ":" + port}).Inc()
 	}
@@ -51,13 +38,14 @@ func (u *User) SaveToRedis(ctx context.Context, rdb *redis.Client, m *metrics, e
 	m.duration.With(prometheus.Labels{"command": "set", "db": "redis", "target": host + ":" + port}).Observe(time.Since(now).Seconds())
 
 	if debug {
-		fmt.Printf("item saved in redis, key: %s, value: %s\n", u.Uuid, string(b))
+		fmt.Printf("item saved in redis, key: %s, value: %s\n", key, value)
 	}
 
-	return err
+	return key, err
 }
 
-func (u *User) GetFromRedis(ctx context.Context, rdb *redis.Client, m *metrics, debug bool) (err error) {
+// GetFromRedis fetches the value for the given key from Redis.
+func GetFromRedis(ctx context.Context, rdb *redis.Client, m *metrics, debug bool, key string) (err error) {
 	now := time.Now()
 	defer func() {
 		if err == nil {
@@ -65,13 +53,13 @@ func (u *User) GetFromRedis(ctx context.Context, rdb *redis.Client, m *metrics, 
 		}
 	}()
 
-	val, err := rdb.Get(ctx, u.Uuid).Result()
+	val, err := rdb.Get(ctx, key).Result()
 	if err != nil {
 		m.requestFailed.With(prometheus.Labels{"command": "get", "db": "redis", "target": host + ":" + port}).Inc()
 	}
 
 	if debug {
-		fmt.Printf("item fetched from redis: %+v\n", val)
+		fmt.Printf("item fetched from redis: key=%s, value=%+v\n", key, val)
 	}
 
 	return err
